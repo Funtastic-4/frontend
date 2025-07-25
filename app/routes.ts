3c883adb72e2a070003421344efd1461444adc31
Fatih Raditya Pratama <allusernameistaken21@gmail.com>
2025-07-25 11:37:39 +0700

    feat: added routes and auto adjustment for screens

--- app/routes.ts
+++ app/routes.ts
@@ -3,4 +3,5 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("cultural-heritage", "routes/cultural-heritage.tsx"),
+  route("cultural-heritage/:siteId", "routes/cultural-heritage.$siteId.tsx"),
 ] satisfies RouteConfig;
